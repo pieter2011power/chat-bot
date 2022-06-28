81c91ab3492e5297f3736a521fd4a2a1b5f7b61c
pieter2011power <61342871+pieter2011power@users.noreply.github.com>
2022-06-28 16:52:27 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,193 @@
+
+import time
+import random
+print('welcome...(made by pieter)')
+time.sleep(0)
+print('loading...(made by pieter)')
+time.sleep(0)
+print('hello...')
+time.sleep(0)
+print('almost finished(made by pieter)')
+
+time.sleep(2)
+
+print("Hi, what's your name?")
+
+
+user_name = input()
+
+print("Nice to meet you, " + user_name)
+
+
+print("What is your favourite movie?")
+
+movie_name = input()
+
+
+comments = ["Oh Nice!", "Thats a good one!", "I haven't seen " + movie_name, "Oh, I hate " + movie_name,
+            "Ive never heard of it.", "Hahaha! thats a funny one", "Wow, me too!"]
+
+
+
+random_comment = random.choice(comments)
+
+
+print(random_comment)
+
+
+print("What is your favourtie letter?")
+
+user_letter = input
+
+
+
+
+response = ["really? I hate that letter", "Nice, Mine is W", "Cool!I like them all :)"]
+
+
+
+
+random_letter_comment = random.choice(response)
+
+
+input()
+
+
+print(random_letter_comment)
+
+
+
+
+print("Okay! Time for a pop quiz!")
+
+letters = input(
+    "How many letters are in the alphabet?: ")
+
+if letters == "26":
+    print("You got it!")
+else:
+    print("Sorry! That is incorrect")
+
+
+legs = input("How many legs does a dog have?: ")
+if legs == "4":
+    print("Nice job :)")
+else:
+    print("I've never seen a dog like that.")
+
+
+temperature = input("Is it colder in the summer or the winter?: ")
+if temperature == "winter":
+    print("Brrrilliant")
+else:
+    print("Whoops! I think you have your seasons mixed up")
+
+
+answer = input("Is it your birthday?")
+if answer == "yes":
+    print("Happy Birthday to you!")
+    print("Happy Birthday to you!")
+    print("Happy Birthday, dear " + user_name)
+
+    import time
+
+    time.sleep(1)
+    print("Happy Birthday to you!")
+# indlude other option for yes
+elif answer == "Yes":
+    print("Happy Birthday to you!")
+    print("Happy Birthday to you!")
+    print("Happy Birthday, dear " + user_name)
+
+    time.sleep(1)
+    print("Happy Birthday to you!")
+
+
+else:
+    input("when is it?")
+    print("okay ill keep that in mind!")
+
+awdada = input ('type 1 if you want go furthur type 2 if you want to exit')
+if awdada  == "2":
+  exit()
+else:
+    print('ok')
+
+
+
+
+
+
+    print("What is your favourite movie?")
+
+
+
+
+
+    movie_name = input()
+
+    comments = ["Oh Nice!", "Thats a good one!", "I haven't seen " + movie_name, "Oh, I hate " + movie_name,
+                "Ive never heard of it.", "Hahaha! thats a funny one", "Wow, me too!"]
+
+    random_comment = random.choice(comments)
+
+    print(random_comment)
+
+    print("What is your favourtie letter?")
+
+    user_letter = input
+
+    response = ["really? I hate that letter", "Nice, Mine is W", "Cool!I like them all :)"]
+
+    random_letter_comment = random.choice(response)
+
+    input()
+
+    print(random_letter_comment)
+
+    print("Okay! Time for a pop quiz!")
+
+    letters = input(
+        "How many letters are in the alphabet?: ")
+
+    if letters == "26":
+        print("You got it!")
+    else:
+        print("Sorry! That is incorrect")
+
+    legs = input("How many legs does a dog have?: ")
+    if legs == "4":
+        print("Nice job :)")
+    else:
+        print("I've never seen a dog like that.")
+
+    temperature = input("Is it colder in the summer or the winter?: ")
+    if temperature == "winter":
+        print("Brrrilliant")
+    else:
+        print("Whoops! I think you have your seasons mixed up")
+
+    answer = input("Is it your birthday?")
+    if answer == "yes":
+        print("Happy Birthday to you!")
+        print("Happy Birthday to you!")
+        print("Happy Birthday, dear " + user_name)
+
+        import time
+
+        time.sleep(1)
+        print("Happy Birthday to you!")
+    # indlude other option for yes
+    elif answer == "Yes":
+        print("Happy Birthday to you!")
+        print("Happy Birthday to you!")
+        print("Happy Birthday, dear " + user_name)
+
+        time.sleep(1)
+        print("Happy Birthday to you!")
+
+
+    else:
+        input("when is it?")
+        print("okay ill keep that in mind!")
+print('by now i need to go (made by pieter')
